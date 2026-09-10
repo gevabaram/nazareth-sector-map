aae73624f220baa4dc419a959b63be8913121efb
gevabaram <gevabaram@gmail.com>
2026-09-10 11:58:51 +0300

    Add Firebase config template

---
+++ firebase-config.example.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "PASTE_FIREBASE_WEB_API_KEY_HERE",
+  authDomain: "polygon-app-e7c11.firebaseapp.com",
+  projectId: "polygon-app-e7c11",
+  storageBucket: "polygon-app-e7c11.firebasestorage.app",
+  messagingSenderId: "410315573550",
+  appId: "PASTE_FIREBASE_APP_ID_HERE"
+};
